9992e557996fee0b440866f12bc8a108f5dca795
Ryan Rodrigues Cordeiro <yryurodriguess@gmail.com>
2025-07-23 14:38:06 -0300

    Add files via upload

--- src/basic-git.ts
+++ src/basic-git.ts
@@ -1,22 +1,33 @@
-// Lessons about git with typescript, just for run. Making this to help news students or any person who want to learn git.
+// Lessons about Git using TypeScript, just for fun.
+// Made to help new students or anyone who wants to learn Git.
 
-//  How to intialize a git repository: Just apply the command git init in your terminal, without = ''.
-const git_init = ''
-// check the status in the terminal
+// How to initialize a Git repository:
+const git_init = 'git init'
+
+// Check the status in the terminal
 const git_status = 'git status'
-const git_add
-// Maker your commit message like this ou any thing in terminal
-const git_commit = 'git commit -m "first commit"'
-// Create a branch for the next step ( Branchs are like a new folder, you can create, delete, or switch between them)
-const git_branch = 'git branch -M main'
-//  Adding a commit and conect your local repository with your remote repository ( github https model and SSH model)
-const git_add_repo_commit_from_your_pc_HTTPS = 'git remote add origin https://github.com/Ryanditko/git4noobs.git'
-const git_add_repo_commit_from_your_pc_SSH = 'git remote add origin git@github.com:Ryanditko/git4noobs.git' 
-// uploading your local repository to remote repository ( github https model and SSH model)
-const git_push = 'git push -u origin main'
 
-// Ty for being here :) 
+// To add your files to the staging area:
+const git_add_file = 'git add <file or path>'
+const git_add_all = 'git add *'
+
+// Make your commit message like this:
+const git_commit = 'git commit -m "first commit"'
+
+// Connect your local repository with your remote GitHub repository:
+const git_remote_HTTPS = 'git remote add origin https://github.com/Ryanditko/git4noobs.git'
+const git_remote_SSH = 'git remote add origin git@github.com:Ryanditko/git4noobs.git'
+
+// Create and switch to a new branch:
+const git_branch = 'git checkout -b nome_da_branch'
+
+// Push your code and set the upstream:
+const git_push_upstream = 'git push --set-upstream origin nome_da_branch'
+
+// Thanks for being here :)
 function Gitlesson() {
-    console.log('bye, now you can start learning git :)')
-};
-Gitlesson() 
+    console.log('Thanks for being here :)');
+    console.log('Bye! Now you can start learning Git');
+}
+
+Gitlesson();
